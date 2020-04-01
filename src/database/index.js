2097530744bdd5d8f0c9cb24531cffe4220c9815
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-04-01 20:35:01 -0300

    Add - Variavel ambiente

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require( 'mongoose' )
+require( 'dotenv/config' )
 
-mongoose.connect( 'mongodb+srv://megahack-covid-19:covid19@cluster0-pudtl.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect( process.env.URL_DATABASE ,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 mongoose.Promise = global.Promise
 
 module.exports = mongoose
